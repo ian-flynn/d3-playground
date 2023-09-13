abceff37f7578c6ea3544cc7c951a855ab1a948c
Ian Flynn <ianflynnwork@gmail.com>
2023-09-13 17:06:22 -0400

    router setup for multipage graphs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createRoot } from 'react-dom/client';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
+import HeatMap from './pages/HeatMap.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'heatmap',
+        element: <HeatMap />,
+      },
+    ],
+  },
+]);
+
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
