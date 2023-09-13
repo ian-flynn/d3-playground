3b628b6a78d209b85140996b38b41fb2e7f39792
Ian Flynn <ianflynnwork@gmail.com>
2023-09-13 18:04:21 -0400

    reemoved imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import * as d3 from 'd3';
-import { useState, useEffect, useRef } from 'react';
 import './index.css';
-import { v4 as uuid } from 'uuid';
-import HeatMap from './pages/HeatMap.jsx';
 import { Link, Outlet } from 'react-router-dom';
 const App = () => {
   return (
     <div id='wrapper'>
       <nav>
         <Link to='/heatmap'>Heat Map</Link>
+        <Link to='/barchart'>Bar Chart</Link>
       </nav>
       <Outlet />
     </div>
