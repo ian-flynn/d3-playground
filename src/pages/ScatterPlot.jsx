3325d59d50c78b4460e1bba7191477937bae320b
Ian Flynn <ianflynnwork@gmail.com>
2024-05-26 12:32:54 -0400

    fixed mutating data issue with json file. Dots placed correctly on scatterplot

--- src/pages/ScatterPlot.jsx
+++ src/pages/ScatterPlot.jsx
@@ -1,35 +1,22 @@
 import * as d3 from 'd3';
 import { useEffect, useState, useRef, useReducer } from 'react';
-import bikeDopingData from '../content/bikeDoping.json';
+import bikeDopingJson from '../content/bikeDoping.json';
 
 const ScatterPlot = ({ width = 800, height = 800, padding = 80 }) => {
-  // const [data, setData] = useState([]);
-  const [timeParsedData, setTimeParsedData] = useState([]);
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const res = await fetch(
-  //       'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
-  //     );
-  //     setData(await res.json());
-  //   }
-  //   fetchData();
-  // }, []);
-
   // Y-Axis
   const gy = useRef();
-  const timeParse = (data) => {
-    return data.map((datum) => {
-      datum = JSON.parse(JSON.stringify(datum));
-      var parsedTime = datum.Time.split(':');
-      datum.Time = new Date(1970, 0, 1, 0, parsedTime[0], parsedTime[1]);
-      return datum;
-    });
-  };
-  const newData = timeParse(bikeDopingData);
+  const bikeDopingData = bikeDopingJson.map((d) => {
+    d = JSON.parse(JSON.stringify(d));
+    const [hours, seconds] = d.Time.split(':');
+    d.Time = new Date(1970, 0, 1, 0, hours, seconds);
+    return d;
+  });
   var yScale = d3
     .scaleTime()
-    .domain([d3.min(newData, (d) => d.Time), d3.max(newData, (d) => d.Time)])
+    .domain([
+      d3.min(bikeDopingData, (d) => d.Time),
+      d3.max(bikeDopingData, (d) => d.Time),
+    ])
     .range([padding, height - padding]);
   useEffect(() => {
     void d3
@@ -55,14 +42,15 @@ const ScatterPlot = ({ width = 800, height = 800, padding = 80 }) => {
 
   // DOTS
   const dots = bikeDopingData.map((datum) => {
+    // console.log(datum);
     return (
       <circle
         r={8}
-        cx={(d) => xScale(d.Year)}
-        cy={10}
-        class='dot'
-        data-xvalue={(d) => d.Year}
-        data-yvalue={(d) => d.Time}
+        cx={xScale(datum.Year)}
+        cy={yScale(datum.Time)}
+        className='dot'
+        data-xvalue={datum.Year}
+        data-yvalue={datum.Time}
       ></circle>
     );
   });
